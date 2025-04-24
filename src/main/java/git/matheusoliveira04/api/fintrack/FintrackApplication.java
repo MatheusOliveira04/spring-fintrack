baa8f42fd1032540ad1ff23b7a586cac510695b7
MatheusOliveira04 <Samatheus999@gmail.com>
2025-04-24 18:50:08 -0300

    feat: initial commit

---
+++ src/main/java/git/matheusoliveira04/api/fintrack/FintrackApplication.java
@@ -0,0 +1,13 @@
+package git.matheusoliveira04.api.fintrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FintrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FintrackApplication.class, args);
+	}
+
+}
