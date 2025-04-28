4d28a972c986a8a52aa99a5262213920938d9e13
MatheusOliveira04 <Samatheus999@gmail.com>
2025-04-28 19:12:25 -0300

    feat: create Role.java class

---
+++ src/main/java/git/matheusoliveira04/api/fintrack/entity/Role.java
@@ -0,0 +1,29 @@
+package git.matheusoliveira04.api.fintrack.entity;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "tb_role")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    @ManyToMany(mappedBy = "roles", fetch = FetchType.LAZY)
+    private Set<User> users = new HashSet<>();
+
+}
