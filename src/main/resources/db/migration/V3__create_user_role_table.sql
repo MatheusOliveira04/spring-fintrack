84476ad390c8c2a91be07ba00a6d53fc92abd19a
MatheusOliveira04 <Samatheus999@gmail.com>
2025-04-28 19:09:57 -0300

    feat(migration): create V3__create_user_role_table.sql

---
+++ src/main/resources/db/migration/V3__create_user_role_table.sql
@@ -0,0 +1,13 @@
+CREATE TABLE IF NOT EXISTS tb_user_role (
+    user_id UUID    NOT NULL,
+    role_id INTEGER NOT NULL,
+    CONSTRAINT pk_tb_user_role PRIMARY KEY (user_id, role_id),
+    CONSTRAINT fk_user_role_user
+      FOREIGN KEY (user_id)
+      REFERENCES tb_user(id)
+      ON DELETE CASCADE,
+    CONSTRAINT fk_user_role_role
+      FOREIGN KEY (role_id)
+      REFERENCES tb_role(id)
+      ON DELETE CASCADE
+);
