765bb05292e9b8858c328e7fb69393643147358e
MatheusOliveira04 <Samatheus999@gmail.com>
2025-04-28 19:12:54 -0300

    feat: create RoleRepository.java class

---
+++ src/main/java/git/matheusoliveira04/api/fintrack/repository/RoleRepository.java
@@ -0,0 +1,14 @@
+package git.matheusoliveira04.api.fintrack.repository;
+
+import git.matheusoliveira04.api.fintrack.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.UUID;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, UUID> {
+
+    List<Role> findByNameIn(List<String> names);
+}
