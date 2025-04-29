55c25846677f5e4179afecae4637ee57106afff5
MatheusOliveira04 <Samatheus999@gmail.com>
2025-04-29 19:47:59 -0300

    feat: created JwtUserDetailsService.java

---
+++ src/main/java/git/matheusoliveira04/api/fintrack/config/jwts/JwtUserDetailsService.java
@@ -0,0 +1,32 @@
+package git.matheusoliveira04.api.fintrack.config.jwts;
+
+import git.matheusoliveira04.api.fintrack.entity.Role;
+import git.matheusoliveira04.api.fintrack.entity.User;
+import git.matheusoliveira04.api.fintrack.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JwtUserDetailsService implements UserDetailsService {
+
+    private UserRepository userRepository;
+
+    public JwtUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        User user = userRepository.findByUsername(username).orElseThrow(null);
+        return org.springframework.security.core.userdetails.
+                User.builder()
+                .username(user.getUsername())
+                .password(passwordEncoder.encode(user.getPassword()))
+                .roles(user.getRoles().stream().map(Role::getName).toArray(String[]::new))
+                .build();
+    }
+}
