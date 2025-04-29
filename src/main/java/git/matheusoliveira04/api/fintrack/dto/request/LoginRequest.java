fd1efa7297c407eb7882c0f3ab3b18affe59023e
MatheusOliveira04 <Samatheus999@gmail.com>
2025-04-29 19:49:07 -0300

    feat: created LoginResponse and LoginRequest

---
+++ src/main/java/git/matheusoliveira04/api/fintrack/dto/request/LoginRequest.java
@@ -0,0 +1,16 @@
+package git.matheusoliveira04.api.fintrack.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class LoginRequest {
+    @NotBlank
+    private String username;
+    @NotBlank
+    private String password;
+}
