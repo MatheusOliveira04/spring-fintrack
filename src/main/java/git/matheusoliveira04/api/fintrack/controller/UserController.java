31e42dfd5561643e353079371205d00a59306e7a
MatheusOliveira04 <Samatheus999@gmail.com>
2025-04-29 19:55:04 -0300

    feat: add @PreAuthorize annotation to endpoint in UserController

--- src/main/java/git/matheusoliveira04/api/fintrack/controller/UserController.java
+++ src/main/java/git/matheusoliveira04/api/fintrack/controller/UserController.java
@@ -1,16 +1,21 @@
 package git.matheusoliveira04.api.fintrack.controller;
 
-import git.matheusoliveira04.api.fintrack.dto.UserRequest;
+import git.matheusoliveira04.api.fintrack.dto.request.UserRequest;
 import git.matheusoliveira04.api.fintrack.entity.Role;
 import git.matheusoliveira04.api.fintrack.entity.User;
 import git.matheusoliveira04.api.fintrack.repository.RoleRepository;
 import git.matheusoliveira04.api.fintrack.repository.UserRepository;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashSet;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/v1/user")
@@ -18,17 +23,21 @@ public class UserController {
 
     private RoleRepository roleRepository;
     private UserRepository userRepository;
+    private BCryptPasswordEncoder passwordEncoder;
 
-    public UserController(RoleRepository roleRepository, UserRepository userRepository) {
+    public UserController(RoleRepository roleRepository, UserRepository userRepository, BCryptPasswordEncoder passwordEncoder) {
         this.roleRepository = roleRepository;
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @PostMapping
+    @Transactional
     public ResponseEntity<User> insert(@RequestBody UserRequest userRequest) {
         Set<Role> role = new HashSet<>(roleRepository.findByNameIn(userRequest.getRoleName().stream().toList()));
         User user = userRepository.save(
-                new User(null, userRequest.getName(), userRequest.getUsername(), userRequest.getPassword(),
+                new User(null, userRequest.getName(), userRequest.getUsername(), passwordEncoder.encode(userRequest.getPassword()),
                         role)
         );
         return ResponseEntity.ok(user);
