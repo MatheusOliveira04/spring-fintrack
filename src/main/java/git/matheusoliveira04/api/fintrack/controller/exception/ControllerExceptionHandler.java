314162595b610e959160f18cf501f9f895527358
MatheusOliveira04 <Samatheus999@gmail.com>
2025-05-03 14:23:03 -0300

    feat: created controller exception

---
+++ src/main/java/git/matheusoliveira04/api/fintrack/controller/exception/ControllerExceptionHandler.java
@@ -0,0 +1,22 @@
+package git.matheusoliveira04.api.fintrack.controller.exception;
+
+import git.matheusoliveira04.api.fintrack.service.exception.UsernameNotFoundException;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.LocalDateTime;
+import java.util.Collections;
+
+@RestControllerAdvice
+public class ControllerExceptionHandler {
+
+    @ExceptionHandler(UsernameNotFoundException.class)
+    public ResponseEntity<StandardError> getUsernameNotFoundException(UsernameNotFoundException exception, HttpServletRequest request) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(
+                new StandardError(LocalDateTime.now(), HttpStatus.NOT_FOUND.value(), request.getRequestURI(), Collections.singletonList(exception.getMessage()))
+        );
+    }
+}
---
+++ src/main/java/git/matheusoliveira04/api/fintrack/controller/exception/StandardError.java
@@ -0,0 +1,18 @@
+package git.matheusoliveira04.api.fintrack.controller.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class StandardError {
+    private LocalDateTime timestamp;
+    private Integer status;
+    private String url;
+    private List<String> messages;
+}
