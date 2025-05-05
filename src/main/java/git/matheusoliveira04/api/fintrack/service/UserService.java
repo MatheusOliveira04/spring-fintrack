ba432dee7b9f803bcfeb3cfb3f4b0ab1595f9097
MatheusOliveira04 <Samatheus999@gmail.com>
2025-05-05 19:27:09 -0300

    feat: created method finById in UserService

--- src/main/java/git/matheusoliveira04/api/fintrack/service/UserService.java
+++ src/main/java/git/matheusoliveira04/api/fintrack/service/UserService.java
@@ -4,8 +4,12 @@ import git.matheusoliveira04.api.fintrack.entity.User;
 import jakarta.validation.constraints.NotNull;
 import org.springframework.validation.annotation.Validated;
 
+import java.util.UUID;
+
 @Validated
 public interface UserService {
 
     User insert(@NotNull User user);
+
+    User findById(@NotNull UUID id);
 }
--- src/main/java/git/matheusoliveira04/api/fintrack/service/impl/UserServiceImpl.java
+++ src/main/java/git/matheusoliveira04/api/fintrack/service/impl/UserServiceImpl.java
@@ -3,9 +3,12 @@ package git.matheusoliveira04.api.fintrack.service.impl;
 import git.matheusoliveira04.api.fintrack.entity.User;
 import git.matheusoliveira04.api.fintrack.repository.UserRepository;
 import git.matheusoliveira04.api.fintrack.service.UserService;
+import git.matheusoliveira04.api.fintrack.service.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.UUID;
 
 @Service
 public class UserServiceImpl implements UserService {
@@ -20,5 +23,12 @@ public class UserServiceImpl implements UserService {
     public User insert(User user) {
         user.setPassword(passwordEncoder.encode(user.getPassword()));
         return userRepository.save(user);
+    }
+
+    @Override
+    public User findById(UUID id) {
+        return userRepository
+                .findById(id)
+                .orElseThrow(() -> new ObjectNotFoundException("User not found with id: " + id));
     }
 }
