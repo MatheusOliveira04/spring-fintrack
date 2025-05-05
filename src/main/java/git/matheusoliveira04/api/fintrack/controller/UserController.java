5b4fcd6961bb0f9593d2271be25475a8e87f2340
MatheusOliveira04 <Samatheus999@gmail.com>
2025-05-05 19:28:13 -0300

    feat: add findById endpoint

--- src/main/java/git/matheusoliveira04/api/fintrack/controller/UserController.java
+++ src/main/java/git/matheusoliveira04/api/fintrack/controller/UserController.java
@@ -1,24 +1,29 @@
 package git.matheusoliveira04.api.fintrack.controller;
 
 import git.matheusoliveira04.api.fintrack.dto.request.UserRequest;
+import git.matheusoliveira04.api.fintrack.dto.response.UserResponse;
 import git.matheusoliveira04.api.fintrack.entity.Role;
 import git.matheusoliveira04.api.fintrack.entity.User;
 import git.matheusoliveira04.api.fintrack.mapper.UserMapper;
 import git.matheusoliveira04.api.fintrack.repository.RoleRepository;
 import git.matheusoliveira04.api.fintrack.repository.UserRepository;
+import git.matheusoliveira04.api.fintrack.service.UserService;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashSet;
 import java.util.Set;
+import java.util.UUID;
 
+@Validated
 @RestController
 @RequestMapping("/api/v1/user")
 public class UserController {
@@ -26,25 +31,25 @@ public class UserController {
     @Autowired
     private RoleRepository roleRepository;
     @Autowired
-    private UserRepository userRepository;
+    private UserService userService;
     @Autowired
     private BCryptPasswordEncoder passwordEncoder;
 
     @Autowired
     private UserMapper userMapper;
 
-    @PreAuthorize("hasRole('ADMIN')")
     @PostMapping
     @Transactional
-    public ResponseEntity<User> insert(@RequestBody UserRequest userRequest) {
+    public ResponseEntity<UserResponse> insert(@RequestBody @NotNull UserRequest userRequest) {
         Set<Role> roles = new HashSet<>(roleRepository.findByNameIn(userRequest.getRoleName().stream().toList()));
+        User user = userService.insert(userMapper.toUser(userRequest, roles));
+        return ResponseEntity.ok(userMapper.toUserResponse(user));
+    }
 
-        var userMapped = userMapper.toUser(userRequest, roles);
-
-        User user = userRepository.save(userMapped
-                //   new User(null, userRequest.getName(), userRequest.getUsername(), passwordEncoder.encode(userRequest.getPassword()),
-                //         role)
-        );
-        return ResponseEntity.ok(user);
+    @PreAuthorize("hasRole('ADMIN')")
+    @GetMapping("/{id}")
+    public ResponseEntity<UserResponse> findById(@PathVariable @NotBlank String id) {
+        User user = userService.findById(UUID.fromString(id));
+        return ResponseEntity.ok(userMapper.toUserResponse(user));
     }
 }
