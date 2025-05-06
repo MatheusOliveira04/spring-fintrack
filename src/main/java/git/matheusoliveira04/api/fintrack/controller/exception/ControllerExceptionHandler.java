3ebf16774fd73d04c740d5d475507eca86f229b0
MatheusOliveira04 <Samatheus999@gmail.com>
2025-05-05 22:07:40 -0300

    fix: adjusted StandardError

--- src/main/java/git/matheusoliveira04/api/fintrack/controller/exception/ControllerExceptionHandler.java
+++ src/main/java/git/matheusoliveira04/api/fintrack/controller/exception/ControllerExceptionHandler.java
@@ -5,6 +5,8 @@ import git.matheusoliveira04.api.fintrack.service.exception.UsernameNotFoundExce
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -26,5 +28,16 @@ public class ControllerExceptionHandler {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(
                 new StandardError(LocalDateTime.now(), HttpStatus.NOT_FOUND.value(), request.getRequestURI(), Collections.singletonList(exception.getMessage()))
         );
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<StandardError> getMethodArgumentNotValidException(MethodArgumentNotValidException exception, HttpServletRequest request) {
+        var allErrors = exception.getAllErrors().stream()
+                .map(errorField -> ((FieldError) errorField).getField() + ": " + errorField.getDefaultMessage())
+                .toList();
+        StandardError error = new StandardError(LocalDateTime.now(), HttpStatus.BAD_REQUEST.value(), request.getRequestURI(), allErrors);
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(error);
     }
 }
--- src/main/java/git/matheusoliveira04/api/fintrack/controller/exception/StandardError.java
+++ src/main/java/git/matheusoliveira04/api/fintrack/controller/exception/StandardError.java
@@ -2,11 +2,13 @@ package git.matheusoliveira04.api.fintrack.controller.exception;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
+@Getter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
