29b75390383f9817c97aee8706401c8d3026a41f
MatheusOliveira04 <samatheus999@gmail.com>
2025-05-12 11:48:24 -0300

    feat: add ConstraintViolationException method in ControllerExceptionHandler

--- src/main/java/git/matheusoliveira04/api/fintrack/controller/exception/ControllerExceptionHandler.java
+++ src/main/java/git/matheusoliveira04/api/fintrack/controller/exception/ControllerExceptionHandler.java
@@ -3,6 +3,7 @@ package git.matheusoliveira04.api.fintrack.controller.exception;
 import git.matheusoliveira04.api.fintrack.service.exception.ObjectNotFoundException;
 import git.matheusoliveira04.api.fintrack.service.exception.UsernameNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.ConstraintViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
@@ -32,12 +33,30 @@ public class ControllerExceptionHandler {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<StandardError> getMethodArgumentNotValidException(MethodArgumentNotValidException exception, HttpServletRequest request) {
-        var allErrors = exception.getAllErrors().stream()
-                .map(errorField -> ((FieldError) errorField).getField() + ": " + errorField.getDefaultMessage())
+        var allErrors = exception.getAllErrors()
+                .stream()
+                .map(errorField -> extractErrorMessage(((FieldError) errorField).getField(), errorField.getDefaultMessage()))
                 .toList();
-        StandardError error = new StandardError(LocalDateTime.now(), HttpStatus.BAD_REQUEST.value(), request.getRequestURI(), allErrors);
         return ResponseEntity
                 .status(HttpStatus.BAD_REQUEST)
-                .body(error);
+                .body(new StandardError(LocalDateTime.now(), HttpStatus.BAD_REQUEST.value(), request.getRequestURI(), allErrors));
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<StandardError> getConstraintViolationException(ConstraintViolationException exception, HttpServletRequest request) {
+        var allErrors = exception.getConstraintViolations()
+                .stream()
+                .map(error -> extractErrorMessage(error.getPropertyPath().toString(), error.getMessage()))
+                .toList();
+        return ResponseEntity.status(HttpStatus.CONFLICT)
+                .body(new StandardError(LocalDateTime.now(), HttpStatus.CONFLICT.value(), request.getRequestURI(), allErrors));
+    }
+
+    private String extractErrorMessage(String fieldName, String message) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(fieldName)
+                .append(": ")
+                .append(message);
+        return sb.toString();
     }
 }
