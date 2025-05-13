08be06112cb589fcb0748e640979d56ff3b2e396
MatheusOliveira04 <samatheus999@gmail.com>
2025-05-13 14:12:14 -0300

    feat: created Entry class

---
+++ src/main/java/git/matheusoliveira04/api/fintrack/entity/Entry.java
@@ -0,0 +1,41 @@
+package git.matheusoliveira04.api.fintrack.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.UUID;
+
+@EqualsAndHashCode(of = "id")
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "tb_entry")
+public class Entry {
+
+    @Id
+    @Setter
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false, length = 50)
+    private String description;
+
+    @Column(nullable = false)
+    private BigDecimal value;
+
+    @Column(nullable = false)
+    private LocalDate date;
+
+    private Boolean paid = false;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private Category category;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+}
