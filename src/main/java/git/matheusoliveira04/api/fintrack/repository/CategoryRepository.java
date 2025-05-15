ff7b1a066c4db5578a6504e199d0c8b19027e180
MatheusOliveira04 <samatheus999@gmail.com>
2025-05-15 15:11:29 -0300

    feat: created CategoryRepository

---
+++ src/main/java/git/matheusoliveira04/api/fintrack/repository/CategoryRepository.java
@@ -0,0 +1,11 @@
+package git.matheusoliveira04.api.fintrack.repository;
+
+import git.matheusoliveira04.api.fintrack.entity.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, UUID> {
+}
