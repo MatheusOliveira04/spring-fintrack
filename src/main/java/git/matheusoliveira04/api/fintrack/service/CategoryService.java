d4ace4f8fbf9503279f716960a2637d5ab163b47
MatheusOliveira04 <samatheus999@gmail.com>
2025-05-15 18:48:49 -0300

    feat: add findById method in CategoryService

--- src/main/java/git/matheusoliveira04/api/fintrack/service/CategoryService.java
+++ src/main/java/git/matheusoliveira04/api/fintrack/service/CategoryService.java
@@ -4,8 +4,12 @@ import git.matheusoliveira04.api.fintrack.entity.Category;
 import jakarta.validation.constraints.NotNull;
 import org.springframework.validation.annotation.Validated;
 
+import java.util.UUID;
+
 @Validated
 public interface CategoryService {
 
     Category insert(@NotNull Category category);
+
+    Category findById(@NotNull UUID id);
 }
--- src/main/java/git/matheusoliveira04/api/fintrack/service/impl/CategoryServiceImpl.java
+++ src/main/java/git/matheusoliveira04/api/fintrack/service/impl/CategoryServiceImpl.java
@@ -3,7 +3,10 @@ package git.matheusoliveira04.api.fintrack.service.impl;
 import git.matheusoliveira04.api.fintrack.entity.Category;
 import git.matheusoliveira04.api.fintrack.repository.CategoryRepository;
 import git.matheusoliveira04.api.fintrack.service.CategoryService;
+import git.matheusoliveira04.api.fintrack.service.exception.ObjectNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.util.UUID;
 
 @Service
 public class CategoryServiceImpl implements CategoryService {
@@ -17,5 +20,11 @@ public class CategoryServiceImpl implements CategoryService {
     @Override
     public Category insert(Category category) {
         return categoryRepository.save(category);
+    }
+
+    @Override
+    public Category findById(UUID id) {
+        return categoryRepository.findById(id)
+                .orElseThrow(() -> new ObjectNotFoundException("Category not found with id: " + id));
     }
 }
