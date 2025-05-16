f181dbdaa838c0b84b820ad8ce606e0d46300f40
MatheusOliveira04 <samatheus999@gmail.com>
2025-05-16 14:14:23 -0300

    refactor: refactor TokenUtil and created method getUser

--- src/main/java/git/matheusoliveira04/api/fintrack/util/TokenUtil.java
+++ src/main/java/git/matheusoliveira04/api/fintrack/util/TokenUtil.java
@@ -1,8 +1,30 @@
 package git.matheusoliveira04.api.fintrack.util;
 
+import git.matheusoliveira04.api.fintrack.config.jwts.JwtUtil;
+import git.matheusoliveira04.api.fintrack.entity.User;
+import git.matheusoliveira04.api.fintrack.service.UserService;
+import org.springframework.stereotype.Component;
+
+@Component
 public class TokenUtil {
 
-    public static String extractBearerCharacters(String token) {
+    private JwtUtil jwtUtil;
+    private UserService userService;
+
+    public TokenUtil(JwtUtil jwtUtil, UserService userService) {
+        this.jwtUtil = jwtUtil;
+        this.userService = userService;
+    }
+
+    public User getUser(String token) {
+        return userService.findByEmail(extractAuthorizationToken(token));
+    }
+
+    private String extractAuthorizationToken(String token) {
+        return jwtUtil.extractUsername(extractBearerCharacters(token));
+    }
+
+    private String extractBearerCharacters(String token) {
         return token.startsWith("Bearer ") ? token.substring(7) : token;
     }
 }
