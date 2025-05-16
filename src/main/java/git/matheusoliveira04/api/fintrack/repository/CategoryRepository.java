b2c149f1125f3109688b2ea65f0d5a33dfcaac34
MatheusOliveira04 <samatheus999@gmail.com>
2025-05-16 14:15:23 -0300

    feat: created findAllByUserId method in CategoryRepository

--- src/main/java/git/matheusoliveira04/api/fintrack/repository/CategoryRepository.java
+++ src/main/java/git/matheusoliveira04/api/fintrack/repository/CategoryRepository.java
@@ -4,8 +4,11 @@ import git.matheusoliveira04.api.fintrack.entity.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, UUID> {
+
+    List<Category> findAllByUserId(UUID id);
 }
