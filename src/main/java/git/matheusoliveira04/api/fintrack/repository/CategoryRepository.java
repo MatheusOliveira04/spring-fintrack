a1d1a3c460c04b79869013000ed2176ff50bf756
MatheusOliveira04 <samatheus999@gmail.com>
2025-05-20 16:06:12 -0300

    refactor: refactored CategoryRepository.findAllByUserId to return a Page<Category>

--- src/main/java/git/matheusoliveira04/api/fintrack/repository/CategoryRepository.java
+++ src/main/java/git/matheusoliveira04/api/fintrack/repository/CategoryRepository.java
@@ -1,6 +1,8 @@
 package git.matheusoliveira04.api.fintrack.repository;
 
 import git.matheusoliveira04.api.fintrack.entity.Category;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,5 +12,5 @@ import java.util.UUID;
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, UUID> {
 
-    List<Category> findAllByUserId(UUID id);
+    Page<Category> findAllByUserId(UUID userId, Pageable pageable);
 }
