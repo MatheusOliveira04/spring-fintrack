2a1a018d528c56c5c5a06443f6b427f9c6099f6a
MatheusOliveira04 <samatheus999@gmail.com>
2025-05-22 13:47:17 -0300

    feat: created method findByIdAndUserId in category repository

--- src/main/java/git/matheusoliveira04/api/fintrack/repository/CategoryRepository.java
+++ src/main/java/git/matheusoliveira04/api/fintrack/repository/CategoryRepository.java
@@ -6,11 +6,13 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, UUID> {
 
     Page<Category> findAllByUserId(UUID userId, Pageable pageable);
+
+    Optional<Category> findByIdAndUserId(UUID categoryId, UUID userId);
 }
