3f7e2b46e7c83be6ba5f27012acc0deebca84a84
MatheusOliveira04 <samatheus999@gmail.com>
2025-05-22 13:46:45 -0300

    feat: added getUserIdByToken method in token util

--- src/main/java/git/matheusoliveira04/api/fintrack/util/TokenUtil.java
+++ src/main/java/git/matheusoliveira04/api/fintrack/util/TokenUtil.java
@@ -3,7 +3,11 @@ package git.matheusoliveira04.api.fintrack.util;
 import git.matheusoliveira04.api.fintrack.config.jwts.JwtUtil;
 import git.matheusoliveira04.api.fintrack.entity.User;
 import git.matheusoliveira04.api.fintrack.service.UserService;
+import git.matheusoliveira04.api.fintrack.service.exception.IntegrityViolationException;
 import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+import java.util.UUID;
 
 @Component
 public class TokenUtil {
@@ -14,6 +18,12 @@ public class TokenUtil {
     public TokenUtil(JwtUtil jwtUtil, UserService userService) {
         this.jwtUtil = jwtUtil;
         this.userService = userService;
+    }
+
+    public UUID getUserIdByToken(String token) {
+        return Optional.ofNullable(getUserByToken(token))
+                .map(User::getId)
+                .orElseThrow(() -> new IntegrityViolationException("The user id does not exist"));
     }
 
     public User getUserByToken(String token) {
