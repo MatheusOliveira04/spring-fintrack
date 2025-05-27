4e864e8c5e8ded2fdc53f91d91f73bf8d831807e
MatheusOliveira04 <samatheus999@gmail.com>
2025-05-27 11:26:08 -0300

    feat: created entry mapper

---
+++ src/main/java/git/matheusoliveira04/api/fintrack/mapper/EntryMapper.java
@@ -0,0 +1,54 @@
+package git.matheusoliveira04.api.fintrack.mapper;
+
+import git.matheusoliveira04.api.fintrack.dto.request.EntryRequest;
+import git.matheusoliveira04.api.fintrack.dto.response.EntryResponse;
+import git.matheusoliveira04.api.fintrack.entity.Category;
+import git.matheusoliveira04.api.fintrack.entity.Entry;
+import git.matheusoliveira04.api.fintrack.entity.User;
+import git.matheusoliveira04.api.fintrack.service.exception.IntegrityViolationException;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+import org.mapstruct.*;
+import org.springframework.validation.annotation.Validated;
+
+import java.util.Optional;
+
+@Validated
+@Mapper(componentModel = MappingConstants.ComponentModel.SPRING,
+unmappedTargetPolicy = ReportingPolicy.ERROR,
+uses = { CategoryMapper.class })
+public interface EntryMapper {
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "category", ignore = true)
+    @Mapping(target = "user", ignore = true)
+    Entry mapToEntry(EntryRequest entryRequest);
+
+    default Entry toEntry(
+            EntryRequest entryRequest,
+            @Context @NotNull @Valid Category category,
+            @Context @NotNull @Valid User user) {
+        Entry entry = mapToEntry(entryRequest);
+        entry.setCategory(category);
+        entry.setUser(user);
+        return entry;
+    }
+
+    @Mapping(target = "category", ignore = true)
+    @Mapping(target = "userId", ignore = true)
+    EntryResponse toEntryResponse(@NotNull Entry entry, @Context CategoryMapper categoryMapper);
+
+    @AfterMapping
+    default void mapToEntryResponse(
+            @MappingTarget EntryResponse entryResponse,
+            @NotNull @Valid Entry entry,
+            @Context CategoryMapper categoryMapper) {
+        Category category = Optional.ofNullable(entry.getCategory())
+                .orElseThrow(() -> new IntegrityViolationException("Category is missing from entry"));
+        User user = Optional.ofNullable(entry.getUser())
+                .orElseThrow(() -> new IntegrityViolationException("User is missing from entry"));
+
+        entryResponse.setCategory(categoryMapper.toCategoryResponse(category));
+        entryResponse.setUserId(user.getId().toString());
+    }
+}
