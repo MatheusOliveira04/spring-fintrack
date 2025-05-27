aa3c250800eb04c1d4fe7ff473020ed55ac61406
MatheusOliveira04 <samatheus999@gmail.com>
2025-05-27 11:26:30 -0300

    feat: created entry controller with insert method

---
+++ src/main/java/git/matheusoliveira04/api/fintrack/controller/EntryController.java
@@ -0,0 +1,60 @@
+package git.matheusoliveira04.api.fintrack.controller;
+
+import git.matheusoliveira04.api.fintrack.dto.request.EntryRequest;
+import git.matheusoliveira04.api.fintrack.dto.response.EntryResponse;
+import git.matheusoliveira04.api.fintrack.entity.Category;
+import git.matheusoliveira04.api.fintrack.entity.Entry;
+import git.matheusoliveira04.api.fintrack.entity.User;
+import git.matheusoliveira04.api.fintrack.mapper.CategoryMapper;
+import git.matheusoliveira04.api.fintrack.mapper.EntryMapper;
+import git.matheusoliveira04.api.fintrack.service.CategoryService;
+import git.matheusoliveira04.api.fintrack.service.EntryService;
+import git.matheusoliveira04.api.fintrack.util.TokenUtil;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/v1/entry")
+public class EntryController {
+
+    private CategoryService categoryService;
+    private CategoryMapper categoryMapper;
+    private EntryService entryService;
+    private EntryMapper entryMapper;
+    private TokenUtil tokenUtil;
+
+    public EntryController(CategoryService categoryService, CategoryMapper categoryMapper, EntryService entryService, EntryMapper entryMapper, TokenUtil tokenUtil) {
+        this.categoryService = categoryService;
+        this.categoryMapper = categoryMapper;
+        this.entryService = entryService;
+        this.entryMapper = entryMapper;
+        this.tokenUtil = tokenUtil;
+    }
+
+    @Operation(security = {@SecurityRequirement(name = "bearer-key")})
+    @PreAuthorize("hasRole('BASIC')")
+    @PostMapping
+    public ResponseEntity<EntryResponse> insert(
+            @RequestBody @Valid EntryRequest entryRequest,
+            @RequestHeader("Authorization") String token,
+            UriComponentsBuilder uriBuilder
+    ) {
+        User user = tokenUtil.getUserByToken(token);
+        Category category = categoryService.findByIdAndUserId(UUID.fromString(entryRequest.getCategoryId()), user.getId());
+        Entry entry = entryMapper.toEntry(entryRequest, category, user);
+        Entry entryInserted = entryService.insert(entry);
+
+        return ResponseEntity
+                .created(uriBuilder.path("/api/v1/entry/{id}").buildAndExpand(entryInserted.getId()).toUri())
+                .body(entryMapper.toEntryResponse(entryInserted, categoryMapper));
+    }
+
+
+}
