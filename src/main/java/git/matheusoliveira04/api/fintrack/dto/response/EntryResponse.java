f6b6604d759d61482b3b0c9b60f945ae454e0c9f
MatheusOliveira04 <samatheus999@gmail.com>
2025-05-27 11:25:40 -0300

    feat: created entry response dto

---
+++ src/main/java/git/matheusoliveira04/api/fintrack/dto/response/EntryResponse.java
@@ -0,0 +1,23 @@
+package git.matheusoliveira04.api.fintrack.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class EntryResponse {
+    private String id;
+    private String description;
+    private BigDecimal value;
+    private LocalDate date;
+    private Boolean paid;
+    private CategoryResponse category;
+    private String userId;
+}
