c9860e23cc9cee99b09eb61746898d972862c265
MatheusOliveira04 <samatheus999@gmail.com>
2025-05-30 15:16:20 -0300

    feat: create method findAllByUserId in EntryService

--- src/main/java/git/matheusoliveira04/api/fintrack/service/EntryService.java
+++ src/main/java/git/matheusoliveira04/api/fintrack/service/EntryService.java
@@ -5,8 +5,13 @@ import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 import org.springframework.validation.annotation.Validated;
 
+import java.util.List;
+import java.util.UUID;
+
 @Validated
 public interface EntryService {
 
     Entry insert(@NotNull @Valid Entry entry);
+
+    List<Entry> findAllByUserId(@NotNull UUID userId);
 }
--- src/main/java/git/matheusoliveira04/api/fintrack/service/impl/EntryServiceImpl.java
+++ src/main/java/git/matheusoliveira04/api/fintrack/service/impl/EntryServiceImpl.java
@@ -3,7 +3,11 @@ package git.matheusoliveira04.api.fintrack.service.impl;
 import git.matheusoliveira04.api.fintrack.entity.Entry;
 import git.matheusoliveira04.api.fintrack.repository.EntryRepository;
 import git.matheusoliveira04.api.fintrack.service.EntryService;
+import git.matheusoliveira04.api.fintrack.service.exception.ObjectNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
 
 @Service
 public class EntryServiceImpl implements EntryService {
@@ -17,5 +21,14 @@ public class EntryServiceImpl implements EntryService {
     @Override
     public Entry insert(Entry entry) {
         return entryRepository.save(entry);
+    }
+
+    @Override
+    public List<Entry> findAllByUserId(UUID userId) {
+        List<Entry> entries = entryRepository.findAllByUserId(userId);
+        if (entries.isEmpty()) {
+            throw new ObjectNotFoundException("No user entries found!");
+        }
+        return entries;
     }
 }
