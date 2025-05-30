d11a8e5ac07db278ca17dfe28938e8e455453bc6
MatheusOliveira04 <samatheus999@gmail.com>
2025-05-30 19:47:34 -0300

    feat: added return page in method findAllByUserId in EntryRepository

--- src/main/java/git/matheusoliveira04/api/fintrack/repository/EntryRepository.java
+++ src/main/java/git/matheusoliveira04/api/fintrack/repository/EntryRepository.java
@@ -1,14 +1,15 @@
 package git.matheusoliveira04.api.fintrack.repository;
 
 import git.matheusoliveira04.api.fintrack.entity.Entry;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface EntryRepository extends JpaRepository<Entry, UUID> {
 
-    List<Entry> findAllByUserId(UUID userId);
+    Page<Entry> findAllByUserId(UUID userId, Pageable pageable);
 }
