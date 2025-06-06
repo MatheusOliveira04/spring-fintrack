f02b4188d8a5196ba8397dd6cf8fff698400349d
MatheusOliveira04 <samatheus999@gmail.com>
2025-06-06 18:27:26 -0300

    feat: implement EntryXlsxImporter to parse XLSX files into EntryRequest objects

---
+++ src/main/java/git/matheusoliveira04/api/fintrack/file/importer/impl/EntryXlsxImporter.java
@@ -0,0 +1,59 @@
+package git.matheusoliveira04.api.fintrack.file.importer.impl;
+
+import git.matheusoliveira04.api.fintrack.dto.request.EntryRequest;
+import git.matheusoliveira04.api.fintrack.file.importer.contract.FileImporter;
+import org.apache.poi.ss.usermodel.CellType;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.springframework.stereotype.Component;
+
+import java.io.InputStream;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Objects;
+
+@Component
+public class EntryXlsxImporter implements FileImporter<EntryRequest> {
+
+    @Override
+    public List<EntryRequest> importFile(InputStream inputStream) throws Exception {
+        try (XSSFWorkbook workbook = new XSSFWorkbook(inputStream)) {
+            XSSFSheet sheet = workbook.getSheetAt(0);
+            Iterator<Row> rowIterator = sheet.iterator();
+
+            if (rowIterator.hasNext()) rowIterator.next();
+
+            return parseRowsToEntryRequestList(rowIterator);
+        }
+    }
+
+    private List<EntryRequest> parseRowsToEntryRequestList(Iterator<Row> rowIterator) {
+        List<EntryRequest> entryRequests = new ArrayList<>();
+
+        while (rowIterator.hasNext()) {
+            Row row = rowIterator.next();
+            if (isNotEmpty(row)) {
+                entryRequests.add(parseRowsToEntryRequest(row));
+            }
+        }
+        return entryRequests;
+    }
+
+    private EntryRequest parseRowsToEntryRequest(Row row) {
+        return EntryRequest.builder()
+                .description(row.getCell(0).getStringCellValue())
+                .value(BigDecimal.valueOf(row.getCell(1).getNumericCellValue()))
+                .date(LocalDate.parse(row.getCell(2).getStringCellValue()))
+                .paid(Boolean.parseBoolean(row.getCell(3).getStringCellValue()))
+                .categoryId(row.getCell(4).getStringCellValue())
+                .build();
+    }
+
+    private static boolean isNotEmpty(Row row) {
+        return Objects.nonNull(row.getCell(0)) && row.getCell(0).getCellType() != CellType.BLANK;
+    }
+}
