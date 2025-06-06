88dd3432fe26bcc4a23a65841e22816158132755
MatheusOliveira04 <samatheus999@gmail.com>
2025-06-06 18:28:13 -0300

    feat: add custom FileProcessingException for file processing errors

---
+++ src/main/java/git/matheusoliveira04/api/fintrack/file/exception/FileProcessingException.java
@@ -0,0 +1,8 @@
+package git.matheusoliveira04.api.fintrack.file.exception;
+
+public class FileProcessingException extends RuntimeException {
+
+    public FileProcessingException(String message) {
+        super(message);
+    }
+}
