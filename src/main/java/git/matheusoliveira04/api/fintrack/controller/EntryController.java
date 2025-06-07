af21a9452ccc51e9548d1422b8109a8d19d715aa
MatheusOliveira04 <samatheus999@gmail.com>
2025-06-07 19:55:30 -0300

    feat: add massInsert method in entry controller

--- src/main/java/git/matheusoliveira04/api/fintrack/controller/EntryController.java
+++ src/main/java/git/matheusoliveira04/api/fintrack/controller/EntryController.java
@@ -21,10 +21,13 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.PositiveOrZero;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.List;
@@ -122,5 +125,16 @@ public class EntryController {
       return ResponseEntity.noContent().build();
     }
 
+     @Operation(security = {@SecurityRequirement(name = "bearer-key")})
+    @PreAuthorize("hasRole('BASIC')")
+    @PostMapping("/massCreation")
+    public ResponseEntity<List<EntryResponse>> massInsert(
+            @RequestParam MultipartFile file,
+            @RequestHeader("Authorization") String token,
+            @PageableDefault(size = 10, page = 0) Pageable pageable) {
+        User user = tokenUtil.getUserByToken(token);
+        List<Entry> entries = entryService.massInsertUsingImportFile(file, user);
+        return ResponseEntity.ok(entryMapper.toEntryResponse(entries, categoryMapper));
+    }
 
 }
